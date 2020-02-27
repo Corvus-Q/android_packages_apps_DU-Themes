2dec5d22428360da7a44d99fc209bd179aeddc97
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-27 21:55:06 +0700

    Update asset/fonts & missing strings for accent

--- src/com/dirtyunicorns/themes/ThemesAdapter.java
+++ src/com/dirtyunicorns/themes/ThemesAdapter.java
@@ -410,31 +410,58 @@ public class ThemesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                 fontType = Typeface.create(Typeface.SERIF, 400, false);
                 break;
             case 3:
-                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Cagliostro.ttf");
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Aclonica.ttf");
                 break;
             case 4:
-                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/LGSmartGothic.ttf");
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Amarante.ttf");
                 break;
             case 5:
-                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Rosemary.ttf");
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Lato.ttf");
                 break;
             case 6:
-                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/SonySketch.ttf");
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Bariol.ttf");
                 break;
             case 7:
-                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/GoogleSans.ttf");
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Cagliostro.ttf");
                 break;
             case 8:
-                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/SlateFromOP.ttf");
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/CircularStd.ttf");
                 break;
             case 9:
-                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/SamsungOne.ttf");
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/ComicSans.ttf");
                 break;
             case 10:
-                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/FiraSans.ttf");
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Coolstory.ttf");
                 break;
             case 11:
-                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Ubuntu.ttf");
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/FiraSans.ttf");
+                break;
+            case 12:
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/GoogleSans.ttf");
+                break;
+            case 13:
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/LGSmartGothic.ttf");
+                break;
+            case 14:
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Rosemary.ttf");
+                break;
+            case 15:
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Rubik.ttf");
+                break;
+            case 16:
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/SamsungOne.ttf");
+                break;
+            case 17:
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/SFProDisplay.ttf");
+                break;
+            case 18:
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/SlateForOnePlus.ttf");
+                break;
+            case 19:
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/SonySketch.ttf");
+                break;
+            case 20:
+                fontType = Typeface.createFromAsset(mResources.getAssets(), "fonts/Surfer.ttf");
                 break;
         }
         return fontType;
